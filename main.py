e675ac1e8dc21acb96a1a4ad2ef7ddaad940c08c
vani <vanipalsingh943@gmail.com>
2026-03-19 11:55:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1 style="text-align:center">My Wish List</h1>
+    <img src="Wish list.jpg" style="display:block;margin: auto;width: 10%;"
+    alt="wishlist">
+    <h3> My Self-improvement List </h3>
+    <ul>
+        <li>Financial Improvement</li>
+        <li>Clutter Clearing</li>
+        <li>Social Interaction</li>
+        <li>Health Improvement</li>
+        <li>Mental Strength Building</li>
+    </ul>
+    <table style="margin: auto;" border=1>
+        <tr>
+            <th>Challenges</th>
+            <th>Opportunitieas</th>
+        </tr>
+        <tr>
+            <td>
+                <ol>
+                    <li>Stop shopping online</li>
+                    <li>Stop dining out</li>
+                    <li>Identify one small area to clean each day</li>
+                    <li>Keep your desk clean and clutter-free</li>
+                    <li>Speak to a stranger</li>
+                    <li>Compliment someone</li>
+                    <li>Exercise for 20 minutes</li>
+                    <li>Go to gym eveyday</li>
+                    <li>Start a gratitude journal</li>
+                    <li>Limit your contact with toxic people</li>
+                    </ol>
+            </td>
+            <td>
+                <ol>
+                    <li>Financial Improvement</li>
 
+                </ol>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
